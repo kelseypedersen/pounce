5348a81e32e4fd77b3d491d5382b07dfa6598107
Alex Heinen <alexheinen99@gmail.com>
2015-07-23 10:43:58 -0700

    build out product#index and product#show routes

--- pounce/app/controllers/products_controller.rb
+++ pounce/app/controllers/products_controller.rb
@@ -14,10 +14,15 @@ class ProductsController < ApplicationController
 
   # Show many items following a search
   def index
+    raw_input = params[:search].to_s
+    formatted_input = raw_input.gsub(" ", "+")
+
+    @products = HTTParty.get("http://api.shopstyle.com/api/v2/products?pid=uid5001-30368749-95&fts='#{formatted_input}'&offset=0&limit=20")
   end
 
   # Details about a particular product
   def show
+     @product = HTTParty.get("http://api.shopstyle.com/api/v2/products/#{params[:id].to_i}?pid=uid5001-30368749-95")
   end
 
   # Used to query database for particular items
