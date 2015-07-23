c624ee7d76b94a3c4f62ce3767b475456c95864e
Alex Heinen <alexheinen99@gmail.com>
2015-07-23 09:37:53 -0700

    add comments to routes to identify what each route will be used for

--- pounce/app/controllers/wants_controller.rb
+++ pounce/app/controllers/wants_controller.rb
@@ -1,17 +1,22 @@
 class WantsController < ApplicationController
 
+  # Post route to create a new want
   def create
   end
 
+  # Get route to view the want before editing
   def edit
   end
 
+  # Updates the want in the database
   def update
   end
 
+  # Delete the want
   def destroy
   end
 
+  # Display all wants for a particular user
   def index
   end
 
