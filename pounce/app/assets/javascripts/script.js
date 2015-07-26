49ad2e33eac02dec9d254538190d9180cf46159d
keioka <koka0828us@gmail.com>
2015-07-25 21:35:26 -0700

    Edit header

--- pounce/app/assets/javascripts/script.js
+++ pounce/app/assets/javascripts/script.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-	$(".signin").leanModal();
-});
+// $(document).ready(function(){
+// 	$(".signin").leanModal();
+// });
+window.onload = function(e){
+	if (window.location.pathname === "/"){
+		  var header = document.getElementsByClassName('header')[0]
+	    var nav = header.getElementsByClassName("button")
+	    Array.prototype.map.call(nav, function(e){e.style.color = "white"; return e })
+	    header.style.backgroundColor = "transparent"
+	    header.style.position = 'absolute';
+	    header.style.color = 'white';
+	}
+}
