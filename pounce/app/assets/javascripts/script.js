a682232081d8928a260a5e993cd25ae11c80404c
keioka <koka0828us@gmail.com>
2015-07-27 10:00:33 -0700

    Add masonry for jquery

--- pounce/app/assets/javascripts/script.js
+++ pounce/app/assets/javascripts/script.js
@@ -1,7 +1,20 @@
 // $(document).ready(function(){
 // 	$(".signin").leanModal();
 // });
+
+$(document).ready(function() {
+  $('#masonry').masonry({}).imagesLoaded(function() {
+	   $('#masonry').masonry({
+	   isFitWidth: true,
+	   columnWidth : 0,
+	   itemSelector: '.item'
+	  });
+  });
+});
+
 window.onload = function(e){
+	var container = document.getElementById("masonry");
+	// container.masonry({});
 	if (window.location.pathname === "/"){
 		  var header = document.getElementsByClassName('header')[0]
 	    var nav = header.getElementsByClassName("button")
@@ -10,7 +23,20 @@ window.onload = function(e){
 	    header.style.position = 'absolute';
 	    header.style.color = 'white';
 	}
+
+	// var container = document.getElementById("masonry");
+	// debugger
+	// container.imagesLoaded(function(){
+	//   container.masonry({
+	//         itemSelector : '.item',
+	//         columnWidth : 300,
+	//         isFitWidth: true
+	// });
+
 }
+
+
+
 
 // window.onbeforeunload = function(e){
 // 	if (window.location.pathname === "/"){
